1451ea8f9e372853311ad0ff3755c46dcf69d8fa
Vishal Singh Shekhawat <vishal22.singh@paytm.com>
2024-01-13 13:16:51 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ClockApp from './ClockApp';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ClockApp />
   </React.StrictMode>
 );
 
